13e85902d4eac494fcfd3d44c7891b77ab78e73a
Ron-Michael Umayan <umayan.ronmichael@gmail.com>
2024-05-15 14:11:21 +0800

    Coverted to Dictionary

--- app.py
+++ app.py
@@ -5,16 +5,31 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
+## FOR LIST
+# def searchStore(items, name):
+#     for item in items:
+#         if(item["name"] == name):
+#             return item
+#     raise RuntimeError("Could not find store")
 
-def searchStore(items, name):
-    for item in items:
-        if(item["name"] == name):
-            return item
-    raise RuntimeError("Could not find store")
+# stores = [
+#     {
+#         "name": "My Store 1",
+#         "items": [
+#             {
+#                 "name": "Chair",
+#                 "price": 15.99
+#             }
+#         ]
 
-stores = [
-    {
-        "name": "My Store 1",
+#     }
+# ]
+
+
+
+stores = {
+    1: {
+        "name": "My Store",
         "items": [
             {
                 "name": "Chair",
@@ -22,9 +37,18 @@ stores = [
             }
         ]
 
-    }
-]
+    },
+    2: {
+        "name": "My Store 2",
+        "items": [
+            {
+                "name": "Table",
+                "price": 71
+            }
+        ]
 
+    }
+}
 
 @app.get("/stores")
 def get_stores(): 
@@ -35,29 +59,29 @@ def get_stores():
 def create_store():
     request_data = request.get_json()
     new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
+    id = len(stores) + 1 
+    stores[id] = new_store
     return { "data": new_store }, 201
 
 
 
 
-@app.post("/stores/<string:name>/items")
-def create_item(name):
+@app.post("/stores/<int:id>/items")
+def create_item(id):
     try:
         request_data = request.get_json()
         new_item = {"name": request_data["name"], "price": request_data["price"]}
-        store = searchStore(stores, name)
-        store["items"].append(new_item)
+        stores[id]["items"].append(new_item)
         return { "data": new_item }, 201
     except RuntimeError as e:
         return {"message": e.__str__()}, 404
 
 
 
-@app.get("/stores/<string:name>")
-def get_store(name): 
+@app.get("/stores/<int:id>")
+def get_store(id): 
     try:
-        store = searchStore(stores, name)
+        store = stores[id]
         return { "data": store } , 200
     except RuntimeError as e:
         return {"message": e.__str__()}, 404
@@ -65,10 +89,10 @@ def get_store(name):
 
 
 
-@app.get("/stores/<string:name>/items")
-def get_store_items(name): 
+@app.get("/stores/<int:id>/items")
+def get_store_items(id): 
     try:
-        store = searchStore(stores, name)
+        store = stores[id]
         return { "data": store["items"] } , 200
     except RuntimeError as e:
         return {"message": e.__str__()}, 404
